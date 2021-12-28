7ee60be69471613f7d64069565037ef07bcaf82a
CyberNinja2007 <cyberninja2019@protonmail.com>
2021-12-28 22:27:22 +0300

    Added Citizen class

---
+++ SimpleApi/Models/Citizen.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleApi.Models
+{
+    public class Citizen
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Sex { get; set; }
+        public string Age { get; set; }
+    }
+}
