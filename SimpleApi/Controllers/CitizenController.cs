6a2cc47d81a8ec1d4bf5ebf3c07784942954733b
CyberNinja2007 <cyberninja2019@protonmail.com>
2022-01-21 23:08:59 +0300

    Updated the CitizenController.cs with the implementation of services and refactored method GetCitizens

--- SimpleApi/Controllers/CitizenController.cs
+++ SimpleApi/Controllers/CitizenController.cs
@@ -1,9 +1,7 @@
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.AspNetCore.Mvc;
 using SimpleApi.Data;
-using SimpleApi.Models;
-using X.PagedList;
+using SimpleApi.Services;
 
 namespace SimpleApi.Controllers
 {
@@ -11,60 +9,28 @@ namespace SimpleApi.Controllers
     [ApiController]
     public class CitizenController : ControllerBase
     {
-        private readonly CitizenContext _context;
+        private readonly ICitizenService _service;
 
         public CitizenController(CitizenContext context)
         {
-            _context = context;
+           _service = new CitizenService(new CitizenRepository(context));
         }
         
         [HttpGet]
-        public IEnumerable<object> GetCitizens(int? page, int minAge = -1, int maxAge = -1, string sex = "")
+        public IEnumerable<object> GetCitizens(int? page,int? numberOfCitizens, int? minAge, int? maxAge, string sex = null)
         {
-            IQueryable<Citizen> citizens;
-
-            if (sex != "")
-            {
-                citizens = _context.Citizens.Where(citizen => citizen.Sex == sex);
-            }
-            else
-            {
-                citizens = _context.Citizens;
-            }
-
-            if (minAge != -1 & maxAge == -1)
-            {
-                citizens = citizens.Where(citizen => citizen.Age > minAge);
-            }
-            else if (maxAge != -1 & minAge == -1)
-            {
-                citizens = citizens.Where(citizen => citizen.Age < maxAge);
-            }
-            else if (maxAge != -1 & minAge != -1)
-            {
-                citizens = citizens.Where(citizen => citizen.Age > minAge & citizen.Age < maxAge);
-            }
-            
-            int pageNum = page ?? 1;
-            int pageSize = 10;
-            
-            return citizens.Select(citizen => new {
-                id = citizen.Id,
-                name = citizen.Name,
-                sex = citizen.Sex
-            }).ToPagedList(pageNum,pageSize);
+            return _service.GetCitizens(page, numberOfCitizens, minAge, maxAge, sex);
         }
-        
+
         [HttpGet("{id}")]
         public object GetCitizen(int id)
         {
-            var citizen = _context.Citizens.Where(citizen => citizen.Id == id).Select(citizen => new {
-                name = citizen.Name,
-                sex = citizen.Sex,
-                age = citizen.Age
-            }).ToArray();
-
-            return citizen;
+            if (id == null)
+            {
+                return BadRequest();
+            }
+            
+            return _service.GetCitizen(id);
         }
     }
 }
