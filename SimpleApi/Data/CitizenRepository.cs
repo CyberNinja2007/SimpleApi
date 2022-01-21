f3554c5a3df80f15b4571a7f0971a9bbffa936fc
CyberNinja2007 <cyberninja2019@protonmail.com>
2022-01-21 23:07:30 +0300

    Added a repository to interact with the data

---
+++ SimpleApi/Data/CitizenRepository.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using SimpleApi.Models;
+
+namespace SimpleApi.Data
+{
+    public class CitizenRepository : ICitizenRepository
+    {
+        private CitizenContext _context;
+        private bool disposed;
+ 
+        public CitizenRepository(CitizenContext context)
+        {
+            _context = context;
+        }
+ 
+        public IEnumerable<object> GetCitizens(int? page,int? numberOfCitizens, int? minAge, int? maxAge, string sex = null)
+        {
+            List<Citizen> citizens;
+
+            citizens = sex != null ? _context.Citizens.Where(citizen => citizen.Sex == sex).ToList() : _context.Citizens.ToList();
+
+            if (minAge != null & maxAge == null)
+            {
+                citizens = citizens.Where(citizen => citizen.Age > minAge).ToList();
+            }
+            else if (maxAge != null & minAge == null)
+            {
+                citizens = citizens.Where(citizen => citizen.Age < maxAge).ToList();
+            }
+            else if (maxAge != null & minAge != null)
+            {
+                citizens = citizens.Where(citizen => citizen.Age > minAge & citizen.Age < maxAge).ToList();
+            }
+            
+            int pageNum = page ?? 1;
+            int pageSize = numberOfCitizens ?? 10;
+            
+            return citizens.Select(citizen => new {
+                id = citizen.Id,
+                name = citizen.Name,
+                sex = citizen.Sex
+            }).Skip(pageNum * pageSize).Take(pageSize).ToList();
+        }
+ 
+        public object GetCitizen(int id)
+        {
+            var citizen = _context.Citizens.Where(citizen => citizen.Id == id).Select(citizen => new {
+                name = citizen.Name,
+                sex = citizen.Sex,
+                age = citizen.Age
+            }).ToArray();
+
+            return citizen;
+        }
+ 
+        public void Create(Citizen citizen)
+        {
+            _context.Citizens.Add(citizen);
+        }
+ 
+        public void Update(Citizen citizen)
+        {
+            _context.Entry(citizen).State = EntityState.Modified;
+        }
+ 
+        public void Delete(int id)
+        {
+            Citizen book = _context.Citizens.Find(id);
+            
+            if(book!=null)
+                _context.Citizens.Remove(book);
+        }
+ 
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+ 
+        public virtual void Dispose(bool disposing)
+        {
+            if(!disposed)
+            {
+                if(disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            disposed = true;
+        }
+ 
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ SimpleApi/Data/ICitizenRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using SimpleApi.Models;
+
+namespace SimpleApi.Data
+{
+    public interface ICitizenRepository : IDisposable
+    {
+        IEnumerable<object> GetCitizens(int? page,int? numberOfCitizens, int? minAge, int? maxAge, string sex = null);
+        object GetCitizen(int id);
+        void Create(Citizen item);
+        void Update(Citizen item);
+        void Delete(int id);
+        void Save();
+    }
+}
