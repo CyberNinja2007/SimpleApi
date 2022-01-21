b83424fc4a38d87995233f9e1c3d8463c642d431
CyberNinja2007 <cyberninja2019@protonmail.com>
2022-01-21 23:08:08 +0300

    Added services to manage the project data

---
+++ SimpleApi/Services/CitizenService.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using SimpleApi.Data;
+using SimpleApi.Models;
+
+namespace SimpleApi.Services
+{
+    public class CitizenService : ICitizenService
+    {
+        private CitizenRepository _repository;
+        
+        public CitizenService(CitizenRepository repository)
+        {
+            _repository = repository;
+        }
+        
+        public IEnumerable<object> GetCitizens(int? page, int? numberOfCitizens, int? minAge, int? maxAge, string sex = null)
+        {
+            return _repository.GetCitizens(page, numberOfCitizens, minAge, maxAge, sex);
+        }
+
+        public object GetCitizen(int id)
+        {
+            return _repository.GetCitizen(id);
+        }
+
+        public void Create(Citizen item)
+        {
+            _repository.Create(item);
+        }
+
+        public void Update(Citizen item)
+        {
+            _repository.Update(item);
+        }
+
+        public void Delete(int id)
+        {
+            _repository.Delete(id);
+        }
+
+        public void Save()
+        {
+            _repository.Save();
+        }
+    }
+}
---
+++ SimpleApi/Services/ICitizenService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using SimpleApi.Models;
+
+namespace SimpleApi.Services
+{
+    public interface ICitizenService
+    {
+        IEnumerable<object> GetCitizens(int? page,int? numberOfCitizens, int? minAge, int? maxAge, string sex = null);
+        object GetCitizen(int id);
+        void Create(Citizen item);
+        void Update(Citizen item);
+        void Delete(int id);
+        void Save();
+    }
+}
